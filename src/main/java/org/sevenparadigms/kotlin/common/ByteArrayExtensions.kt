5500dfc4276f8feb1dee2dd2b76335bd92a37048
Lao Tsing <lao.tsing@outlook.com>
2021-09-15 17:44:30 +0300

    up version

--- src/main/java/org/sevenparadigms/kotlin/common/ByteArrayExtensions.kt
+++ src/main/java/org/sevenparadigms/kotlin/common/ByteArrayExtensions.kt
@@ -1,6 +1,7 @@
 package org.sevenparadigms.kotlin.common
 
 import org.springframework.util.ResourceUtils
+import java.nio.charset.Charset
 import java.nio.file.Files
 import java.util.*
 
@@ -9,3 +10,5 @@ fun ByteArray.encode() = Base64.getMimeEncoder().encode(this)
 fun ByteArray.decode() = Base64.getMimeDecoder().decode(this)
 
 fun String.loadResource() = Files.readAllBytes(ResourceUtils.getFile("classpath:$this").toPath())
+
+fun ByteArray.convertString(): String = String(this, Charsets.UTF_8)
