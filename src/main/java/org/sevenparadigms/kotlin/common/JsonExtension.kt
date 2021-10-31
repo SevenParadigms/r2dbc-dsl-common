4408a2112b171d09918ad73b42246cab55fd8ba6
Lao Tsing <lao.tsing@outlook.com>
2021-10-31 16:02:38 +0300

    add array node extension

--- src/main/java/org/sevenparadigms/kotlin/common/JsonExtension.kt
+++ src/main/java/org/sevenparadigms/kotlin/common/JsonExtension.kt
@@ -1,6 +1,8 @@
 package org.sevenparadigms.kotlin.common
 
 import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.ArrayNode
+import org.apache.commons.beanutils.ConvertUtils
 import org.springframework.data.r2dbc.support.JsonUtils
 
 fun Any.toJsonNode(): JsonNode = JsonUtils.objectToJson(this)
@@ -10,5 +12,17 @@ fun <T> String.parseJson(cls: Class<T>): T = JsonUtils.stringToObject(this, cls)
 fun <T> JsonNode.parseJson(cls: Class<T>): T = JsonUtils.jsonToObject(this, cls)
 
 fun <T> JsonNode.parseArray(cls: Class<T>): List<T> = JsonUtils.jsonToObjectList(this, cls)
+
+fun String?.toArrayNode(): ArrayNode = if (this == null)
+    JsonUtils.arrayNode()
+else
+    JsonUtils.getMapper().readTree(this) as ArrayNode
+
+fun <T> ArrayNode.contains(value: T): Boolean {
+    for (node in this)
+        if (node.asText().equals(ConvertUtils.convert(value, String::class.java).toString()))
+            return true
+    return false
+}
 
 fun JsonNode.singleQuotes(): String = this.toString().replace("\"", "'")
