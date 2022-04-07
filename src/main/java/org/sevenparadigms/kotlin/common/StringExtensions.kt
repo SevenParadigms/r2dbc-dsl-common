46a32ba4051c8b75b9f49237bd612beb4d900a29
Lao Tsing <lao.tsing@outlook.com>
2022-04-07 18:22:08 +0300

    up depends

--- src/main/java/org/sevenparadigms/kotlin/common/StringExtensions.kt
+++ src/main/java/org/sevenparadigms/kotlin/common/StringExtensions.kt
@@ -4,6 +4,12 @@ import org.springframework.data.r2dbc.support.WordUtils
 
 fun String.remove(regex: String): String = this.replace(regex.toRegex(), "")
 
+fun String.removeBefore(template: String): String =
+    if (this.contains(template)) this.substring(this.lastIndexOf(template) + template.length) else this
+
+fun String.removeAfter(template: String): String =
+    if (this.contains(template)) this.substring(0, this.indexOf(template)) else this
+
 fun String.camelToSql(camel: String): String = WordUtils.camelToSql(camel)
 
 fun String.sqlToCamel(sqlName: String): String = WordUtils.sqlToCamel(sqlName)
